4d5d1a0683f9ee643094b88e82cc02ff0b3c7612
shikhar2333 <shikhar.shasya@research.iiit.ac.in>
2020-05-28 19:47:59 +0530

    Implemented basic signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+    before_save { self.email = self.email.downcase }
     validates :name,  presence: true, length: { minimum:4, maximum: 50 }
-    VALID_EMAIL_REGEX = /\A^[\w+-\.]+@([\w+-]+\.)+[\w-]{2,4}\z/
+    VALID_EMAIL_REGEX = /\A[\w+-\.]+@([\w+-]+\.)+[\w-]{2,4}\z/
     validates :email,  presence: true, length: { maximum: 255 },
-                       format: {with: VALID_EMAIL_REGEX}
+                       format: {with: VALID_EMAIL_REGEX},
+                       uniqueness: true
+    has_secure_password
+    # VALID_PHONE_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}\z/
+    # validates :phone_number, length:{maximum: 10}, format: {with: VALID_PHONE_REGEX}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_211801) do
+ActiveRecord::Schema.define(version: 2020_05_28_135939) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "phone_number"
   end
 
 end
