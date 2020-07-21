11ddcbfe1420197887d5cbd5af17a4e6124cd891
shikhar2333 <shikh5030@gmail.com>
2020-07-21 17:49:22 +0530

    Edited view and delete action for scenario controller

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,10 +1,44 @@
 class ScenariosController < ApplicationController
-    # before_action :set_scenario
+  before_action :set_scenario, only: [:show, :edit, :update, :destroy]
     include ScenariosHelper
     def new
       @scenario = Scenario.new
-      # puts @scenario
     end
+
+    def index
+      @scenarios = Scenario.all 
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @scenario.update(scenario_params)
+          format.html { redirect_to @scenario, notice: 'Scenario was successfully updated.' }
+          # format.json { render :show, status: :ok, location: @scenario }
+        # else
+        #   format.html { render :edit }
+        #   format.json { render json: @scan_image.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @scenario.destroy
+      respond_to do |format|
+        s3 = Aws::S3::Resource.new
+        bucket_name = get_bucket_name(@scenario)
+        bucket = s3.bucket(bucket_name)
+        bucket.delete!
+        format.html { redirect_to scenarios_url, notice: 'Scenario was successfully destroyed.' }
+        # format.json { head :no_content }
+      end
+    end
+
     def create
       @scenario = Scenario.new(scenario_params)
       @scenario.user_id = current_user.id
@@ -12,7 +46,6 @@ class ScenariosController < ApplicationController
       if @scenario.save!
         @scenario.static_website.attach(params[:scenario][:static_website])
         website_name = @scenario.static_website.filename
-        puts "Status:" + @scenario.static_website.attached?.to_s
         if @scenario.static_website.attached?
             Dir.mktmpdir do |dir|
               website_attachment_path = "#{dir}/#{website_name}"
@@ -22,7 +55,8 @@ class ScenariosController < ApplicationController
               unzip_file(website_attachment_path, dir)
               File.delete(website_attachment_path) if File.exist?(website_attachment_path)
               puts "Dir:" + dir.to_s
-              return_new_bucket(dir, website_name, @scenario)
+              s3_link = return_new_bucket(dir, website_name, @scenario)
+              @scenario.update_attribute(:link_to_scenario, s3_link.to_s)
               puts "My new website: #{website_attachment_path}"
             end
             flash[:success] = "AR scenario successfully uploaded"
@@ -35,9 +69,9 @@ class ScenariosController < ApplicationController
   
   
     private
-      # def set_scenario
-      #   @scenario = Scenario.find(params[:id])
-      # end
+      def set_scenario
+        @scenario = Scenario.find(params[:id])
+      end
       def scenario_params
         params.require(:scenario).permit(:title, :static_website)
       end
--- app/helpers/scenarios_helper.rb
+++ app/helpers/scenarios_helper.rb
@@ -12,30 +12,40 @@ module ScenariosHelper
            end
         end
     end
+    def get_bucket_name(scenario)
+        return "youngsphere-#{current_user.name.to_s}-#{scenario.id}"
+    end
     def return_new_bucket (dir, bucket, scenario)
-        bucket = "youngsphere-#{current_user.name.to_s.downcase!}-#{scenario.id}"
+        bucket = get_bucket_name(scenario)
         profile_name = 'JinMoRi'
-        puts "Bucket" + bucket.to_s
+        s3_link = ''
         s3 = Aws::S3::Client.new(profile: profile_name, region: ENV['AWS_REGION'])
         s3.create_bucket(bucket: bucket)
         uploader = S3Uploader::Uploader.new({
             :s3_key => ENV['AWS_ACCESS_KEY'],
             :s3_secret => ENV['AWS_SECRET_KEY'],
-            :destination_dir => 'app',
+            :destination_dir => '',
             :region => ENV['AWS_REGION'],
             :threads => 10
            })
         uploader.upload(dir, bucket)
         bucket_contents = s3.list_objects(bucket: bucket).contents
-        aws_folder = "app"
+        aws_folder = ""
         acl = 'public-read'
         files = bucket_contents.map{ |f| "#{f.key}" if f.key.include?(aws_folder) }.compact
         files.each do |obj_key|
+            key = obj_key.to_s
+            if key.include? "index.html"
+                s31 = Aws::S3::Resource.new
+                new_bucket = s31.bucket(bucket)
+                s3_link = new_bucket.object(key).public_url
+            end
             s3.put_object_acl(
               key: obj_key,          # object(file) name
               bucket: bucket,
               acl: acl               # 'public-read'
             )
         end
+        return s3_link
     end
 end
